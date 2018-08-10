23cdb6b793331e806d20421c0aa703ce9577d877
Carlos Perez <solracias@gmail.com>
2018-08-10 12:51:42 -0700

    user can now login via the api with validation checks

--- routes/api/users.js
+++ routes/api/users.js
@@ -53,4 +53,31 @@ router.post("/register", (req, res) => {
   });
 });
 
+/**
+ * @route   GET localhost:5000/api/users/login
+ * @desc Login User and return JWT token
+ * @access Public
+ */
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // find user by email
+  User.findOne({ email: email }).then(user => {
+    // check for user
+    if (!user) {
+      return res.status(404).json({ email: "user not found" });
+    }
+
+    // Check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: "Success" });
+      } else {
+        return res.status(400).json({ password: "Password incorrect" });
+      }
+    });
+  });
+});
+
 module.exports = router;
