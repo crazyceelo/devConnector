f5a1bab8f4e62edbb780a49fdfbdff23175124cf
Carlos Perez <solracias@gmail.com>
2018-08-13 12:24:55 -0700

    created public api routest for profile by handle and user_id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -47,6 +47,46 @@ router.get(
 );
 
 /**
+ * @route    GET localhost:5000/api/user/:user_id
+ * @desc     Get profile by user ID
+ * @access   public
+ */
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+/**
+ * @route    GET localhost:5000/api/profile/handle/:handle
+ * @desc     Get profile by handle
+ * @access   public
+ */
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user";
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+/**
  * @route    POST localhost:5000/api/profile
  * @desc     Create or edit user profile
  * @access   private
