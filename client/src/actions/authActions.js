0f63e6c0a7cfcbdb0405ae7e25a4d8854f3ce728
Carlos Perez <solracias@gmail.com>
2018-08-20 12:45:48 -0700

    login with redux works. front-end not done yet.

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,7 +1,14 @@
 import { GET_ERRORS } from "./types";
+import { SET_CURRENT_USER } from "./types";
 import axios from "axios";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
 
-// Register User
+/**
+ * Register User
+ * @param {Object} userData - name, email, pass 1, pass 2
+ * @param {Array} history - array of previous urls
+ */
 export const registerUser = (userData, history) => dispatch => {
   axios
     .post("/api/users/register", userData)
@@ -12,4 +19,36 @@ export const registerUser = (userData, history) => dispatch => {
         payload: err.response.data
       })
     );
+};
+
+// Login - Get user token
+export const loginUser = userData => dispatch => {
+  axios
+    .post("/api/users/login", userData)
+    .then(res => {
+      // save to localStorage
+      const { token } = res.data;
+      // Set token to localStorage
+      localStorage.setItem("jwtToken", token);
+      // Set token to Auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
 };
