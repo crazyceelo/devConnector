2ba785b6a3e8499b8e0a5c13f7f1383af3e8508a
Carlos Perez <solracias@gmail.com>
2018-08-27 13:09:28 -0700

    add experience component started and in progress

--- client/src/App.js
+++ client/src/App.js
@@ -27,6 +27,7 @@ import Dashboard from "./components/dashboard/Dashboard";
 import { clearCurrentProfile } from "./actions/profileActions";
 import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
+import AddExperience from "./components/add-credentials/AddExperience";
 
 // check for token
 if (localStorage.jwtToken) {
@@ -74,6 +75,13 @@ class App extends Component {
                   exact
                   path="/edit-profile"
                   component={EditProfile}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-experience"
+                  component={AddExperience}
                 />
               </Switch>
             </div>
