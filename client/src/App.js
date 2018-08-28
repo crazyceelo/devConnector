97455ddbc47f59437d48f77fd35a1b601d7c9e14
Carlos Perez <solracias@gmail.com>
2018-08-27 20:43:02 -0700

    user is now able to add education

--- client/src/App.js
+++ client/src/App.js
@@ -28,6 +28,7 @@ import { clearCurrentProfile } from "./actions/profileActions";
 import CreateProfile from "./components/create-profile/CreateProfile";
 import EditProfile from "./components/edit-profile/EditProfile";
 import AddExperience from "./components/add-credentials/AddExperience";
+import AddEducation from "./components/add-credentials/AddEducation";
 
 // check for token
 if (localStorage.jwtToken) {
@@ -82,6 +83,13 @@ class App extends Component {
                   exact
                   path="/add-experience"
                   component={AddExperience}
+                />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/add-education"
+                  component={AddEducation}
                 />
               </Switch>
             </div>
