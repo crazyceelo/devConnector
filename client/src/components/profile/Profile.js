5d97ff7c08b413a0ce81d0466c623c85b1797b1c
Carlos Perez <solracias@gmail.com>
2018-08-28 22:33:51 -0700

    now able to view experience and education

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -37,7 +37,10 @@ class Profile extends Component {
           </div>
           <ProfileHeader profile={profile} />
           <ProfileAbout profile={profile} />
-          <ProfileCreds profile={profile} />
+          <ProfileCreds
+            education={profile.education}
+            experience={profile.experience}
+          />
           <ProfileGithub profile={profile} />
         </div>
       );
