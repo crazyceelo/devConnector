c63588dce2684d1a6e3185c221a790c69e3acfa0
Carlos Perez <solracias@gmail.com>
2018-08-28 20:48:45 -0700

    able to view a single profile from a public route

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -30,6 +30,27 @@ export const getCurrentProfile = () => dispatch => {
 };
 
 /**
+ * gets current user and starts loading of profile
+ */
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: {}
+      })
+    );
+};
+
+/**
  * Create Profle
  */
 export const createProfile = (profileData, history) => dispatch => {
