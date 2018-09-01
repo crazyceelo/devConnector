feefc1cd8fc4623720ed5089faa6cd61301f6b15
Carlos Perez <solracias@gmail.com>
2018-09-01 11:39:35 -0700

    created build folder and set server to point to build when in production mode

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 // import internal files
 const users = require("./routes/api/users");
@@ -37,6 +38,17 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  // for any route hit, load the build index.html file isntead of dev index.html
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // variable for production port or local port
 const port = process.env.PORT || 5000;
