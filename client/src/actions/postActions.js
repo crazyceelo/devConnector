8f432dbc688b42319875886ecb0f9ca488f8f6a3
Carlos Perez <solracias@gmail.com>
2018-08-31 23:42:02 -0700

    user is now able to delete comments and clear errors globally

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -5,7 +5,8 @@ import {
   GET_POSTS,
   GET_POST,
   POST_LOADING,
-  DELETE_POST
+  DELETE_POST,
+  CLEAR_ERRORS
 } from "./types";
 
 /**
@@ -13,6 +14,7 @@ import {
  * @param {text, name, avatar, user} postData
  */
 export const addPost = postData => dispatch => {
+  dispatch(clearErrors());
   axios
     .post("/api/posts", postData)
     .then(res =>
@@ -29,9 +31,32 @@ export const addPost = postData => dispatch => {
     );
 };
 
+/**
+ * add a comment
+ * @param {object} postId
+ * @param {object} commentData
+ */
 export const addComment = (postId, commentData) => dispatch => {
+  dispatch(clearErrors());
   axios
     .post(`/api/posts/comment/${postId}`, commentData)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+export const deleteComment = (postId, commentId) => dispatch => {
+  axios
+    .delete(`/api/posts/comment/${postId}/${commentId}`)
     .then(res =>
       dispatch({
         type: GET_POST,
@@ -143,4 +168,13 @@ export const removeLike = id => dispatch => {
  */
 export const setPostLoading = () => {
   return { type: POST_LOADING };
+};
+
+/**
+ * Clear errors
+ */
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS
+  };
 };
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,6 +5,7 @@ import PropTypes from "prop-types";
 import Spinner from "../common/Spinner";
 import { Link } from "react-router-dom";
 import CommentForm from "./CommentForm";
+import CommentFeed from "./CommentFeed";
 
 // actions
 import { getPost } from "../../actions/postActions";
@@ -26,6 +27,7 @@ class Post extends Component {
         <div>
           <PostItem post={post} showActions={false} />
           <CommentForm postId={post._id} />
+          <CommentFeed postId={post._id} comments={post.comments} />
         </div>
       );
     }
