a020d6974e36944ef72520a65397a9e967a68224
Carlos Perez <solracias@gmail.com>
2018-09-01 11:23:33 -0700

    keys are now hidden added to gitignore file

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://carlos:Berserk101!@ds159208.mlab.com:59208/solrak",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
